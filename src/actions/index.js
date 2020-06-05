16d49c765c766342008898584403ab0f5e87a183
Cean Tolentino <cean.tolentino@gmail.com>
2020-06-05 10:20:14 -0700

    first commit

---
+++ src/actions/index.js
@@ -0,0 +1,15 @@
+import jsonPlaceholder from '../apis/jsonPlaceholder';
+
+export const fetchPosts = () => async dispatch => {
+   const response = await jsonPlaceholder.get('/posts');
+
+   dispatch({
+      type: 'FETCH_POSTS',
+      payload: response.data
+   });
+   /* BAD APPROACH!!
+   const response = await jsonPlaceholder.get(
+      '/posts'
+   );
+   */
+};
